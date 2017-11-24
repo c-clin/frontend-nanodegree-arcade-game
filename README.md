2b256087feec50ecac19b9fc2dca13e99ba467c3
Catherine Lin <catherine.lin.93@gmail.com>
2017-11-24 11:06:16 -0800

    Added different rewards for the Collectibles and edited the README

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # Arcade Frogger Clone Project
 
-## Getting Started
-
-
 ### Installing
-
+Download the repository and unzip the file. Open `index.html` to start playing.
 
 ### Play the game
-
+* The objective of the game is for the player to cross the road and reach the water (earning 1000 points each time). 
+* The player must earn a minimum of 10000 points to win the game. 
+* The keyboard arrow keys can be used to navigate through the moving obsticles.
+* Colliding with the obsticles will decrease the number of lives. 
+* The player loses the game when their life reaches 0. 
 
 ### Special Features
-Below are the list of collectibles available and their function:
-* Heart: +1 live 
-* Gem: +500 score
-* Star: +1000 score
+Below are the list of collectibles available and their functions:
+* Heart: +1 life 
+* Gem: +300 score
+* Star: +800 score
 * Rock: -500 score
 
 
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let lives = 5;
-let score = 0;
+let  score = 0;
 
 // Renders the current lives and score on the page
 $(".lives").html(lives);
@@ -10,12 +10,11 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// xy coordinates for the collectibles
+// Possible xy coordinates for the collectibles
 horizontals = [-1000, 105, 205, 305, 405, 505, 605];
-verticals = [-1000, 40, 125, 201 ,295];
+verticals = [-1000, 75, 160, 245 ,330];
 
-// Enemies our player must avoid
-
+// The Enemy class
 class Enemy {
     constructor (x, y, speed) {
         this.sprite = 'images/enemy-bug.png';
@@ -23,42 +22,36 @@ class Enemy {
         this.y = y;
         this.speed = speed;
     }
-    // Update the enemy's position, required method for game
-    // Parameter: dt, a time delta between ticks
+
     update(dt) {
-      // You should multiply any movement by the dt parameter
-      // which will ensure the game runs at the same speed for
-      // all computers.
-
-      //this.x++
-      this.x += this.speed * dt;
-
-      if (this.x > ctx.canvas.width) this.x = -100;
-
-      this.checkCollision();
-
+        // Sets up the speed 
+        this.x += this.speed * dt;
+        // Creates the loop 
+        if (this.x > ctx.canvas.width) this.x = -100;
+        this.checkCollision();
     }    
+
     // Draw the enemy on the screen, required method for game
     render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y, 90, 130);
     }
 
     checkCollision() {
-      // https://developer.mozilla.org/kab/docs/Games/Techniques/2D_collision_detection
+    // Enemy rectangle
       var rect1 = {
         x: this.x,
         y: this.y,
         width: 40,
         height: 40
       };
-
+    // Player rectangle
       var rect2 = {
         x: player.x,
         y: player.y,
         width: 40,
         height: 40
       };
-
+      // When enemy and playe rectangle collides
       if (
         rect1.x < rect2.x + rect2.width &&
         rect1.x + rect1.width > rect2.x &&
@@ -66,17 +59,15 @@ class Enemy {
         rect1.height + rect1.y > rect2.y
       ) {
         console.log("Collision detected, player dies");
-        player.reset();
-        collectible.setPlacement();
+        player.reset(); 
+        collectible.setPlacement(); // Reset the colletible position
         lives--;
         $(".lives").html(lives);
       }
     } 
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// The Player class
 class Player {
     constructor(x, y) {
         this.sprite = 'images/char-horn-girl.png';
@@ -111,13 +102,12 @@ class Player {
                 this.y = 465;
             }
             else {
-                this.y = -45;
+                this.y =- 45;
             }
         }   
     }   
 
     handleInput(event) {
-
         switch (event) {
             case "left":
               this.x -= 100;
@@ -132,10 +122,6 @@ class Player {
               this.y += 85;
               break;
         }
-        // going left = x - 100
-        // going right = x + 100
-        // going up = y - 85
-        // going down = y + 85
     }
 
     reset() {
@@ -148,18 +134,18 @@ class Player {
     score() {
         if (this.y < 0) {
             this.reset(); 
-            score = score + 1000;
+            collectible.setPlacement(); // Reset collectible position
+            score += 1000;
             $(".score").html(score);
-            console.log('Score: ' + score);
-            collectible.setPlacement();
-            console.log("Play wins game");
+            console.log("Player scores!");
         }
     }
 
     // When player wins the game
     winGame() {
         if (score >= 10000) {
-            console.log("Player wins game!!!");
+            this.reset();
+            this.y = this.y + 7; // Keeps the player on the grass
             $("#game-over").html("YOU WON!");
             $("#hidden").css("display", "block"); // Displays the game over box
         }
@@ -168,17 +154,16 @@ class Player {
     // When player loses the game
     loseGame() {
         if (lives === 0) {
-            console.log("player loses game!!!");
-            this.y = this.y + 3; // Wants the character to fall of the grid when loses, need to stop player.reset()
+            this.y = this.y + 7; // Keeps the player on the grass
             $("#game-over").html("GAME OVER");
             $("#hidden").css("display", "block"); // Displays the game over box
         }
     }
-
 }
 
-let collectiblesArray = ["images/Gem-Orange.png", "images/Gem-Orange.png", "images/Heart.png", "images/Heart.png", "images/Star.png", "images/Rock.png"];
-// Create a new class for collectibles
+// An array containing all the possible collectibles
+let collectiblesArray = ["images/Gem-Orange.png", "images/Heart.png", "images/Star.png", "images/Rock.png"];
+
 class Collectibles {
     constructor(x, y) {
         this.sprite = collectiblesArray[getRandomInt(0,1)]; // Randomly pics between gem and heart
@@ -187,25 +172,25 @@ class Collectibles {
     }
 
     update(dt) {
-        // Call the collision function
         this.setCollision();
         if (this.x > ctx.canvas.width) this.x = -1000; // Prevents the collectibles from showing outside of the canvas
         if (this.y > ctx.canvas.width) this.y = -1000;
     }
 
     render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y, 80, 100);
     }
 
-    // Randomize the placement of the collectible
+    // Method to randomize the placement of the collectible
     setPlacement() {
-        // Randomize the index number picked from the relative arrays
+        // Randomize the xy coordinates from the array defined above
         let horizontal = horizontals[getRandomInt(0,horizontals.length-1)];
         let vertical = verticals[getRandomInt(0,verticals.length-1)];
         console.log("collectible x: " + horizontal);
         console.log("collectible y: " + vertical);
         this.sprite = collectiblesArray[getRandomInt(0, collectiblesArray.length-1)];
-        // Set the placement equal to the randomized placement
+        console.log("sprite: " + this.sprite);
+        // Set the placement equal to the randomized coordinates
         this.x = horizontal;
         this.y = vertical;
     }
@@ -234,31 +219,30 @@ class Collectibles {
         rect1.x + rect1.width > rect2.x &&
         rect1.y < rect2.y + rect2.height &&
         rect1.height + rect1.y > rect2.y
-        ) {
-            console.log("Collision detected");
+        ) { 
             if (this.sprite === "images/Heart.png") {
                 lives++; // Player's life +1 when collect the heart
                 $(".lives").html(lives);
                 console.log("Player collects heart")
                 this.setPlacement(); 
             }
-            if (this.sprite === "images/Star.png") {
-                score = score + 1000; // Player score +1000 when collect the star 
+            else if (this.sprite === "images/Star.png") {
+                score += 800; // Player score +800 when collect the star 
                 $(".score").html(score);
                 console.log("Player collects star");
                 this.setPlacement();
             }
-            if (this.sprite === "images/Rock.png") {
-                score = score - 500; // Player score -500 when collect the rock 
+            else if (this.sprite === "images/Rock.png") {
+                score -= 500; // Player score -500 when collect the rock 
                 $(".score").html(score);
                 console.log("Player collects rock");
                 this.setPlacement();
             }
-            else {
-                score = score + 500; // Player score +500 when collect the gem 
+            else if (this.sprite === "images/Gem-Orange.png") {
+                score += 300; // Player score +300 when collect the gem 
                 $(".score").html(score);
                 console.log("Player collects gem");
-                this.setPlacement();
+                this.setPlacement();                
             }
         }
     }
@@ -269,22 +253,20 @@ const player = new Player(305, 465);
 
 // Creates the enemies objects
 allEnemies = [];
-const enemy1 = new Enemy(5, 40, 300); 
-const enemy11 = new Enemy(5, 40, 550);
-const enemy2 = new Enemy(5, 125, 450);
-const enemy3 = new Enemy(5, 210, 350);
-const enemy33 = new Enemy(5, 210, 600);
-const enemy4 = new Enemy(5, 295, 250);
-const enemy44 = new Enemy(5, 295, 500);
+const enemy1 = new Enemy(5, 75, 300); 
+const enemy11 = new Enemy(5, 75, 550);
+const enemy2 = new Enemy(5, 160, 450);
+const enemy3 = new Enemy(5, 245, 350);
+const enemy33 = new Enemy(5, 245, 600);
+const enemy4 = new Enemy(5, 330, 250);
+const enemy44 = new Enemy(5, 330, 500);
 // Adds the enemy objects to the enemies array
 allEnemies.push(enemy1, enemy11, enemy2, enemy3, enemy33, enemy4, enemy44);
 
-
 // Create the collectible objects
-const collectible = new Collectibles(105,125);
+const collectible = new Collectibles(105,160);
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -299,26 +281,14 @@ document.addEventListener('keyup', function(e) {
     }, 1000);
 });
 
-
 // Allows the player to click play again to restart the game
 $("#play-again").click (function() {
-    lives = 5; // Reset the player's lives
-    score = 0; // Reset the player's score
+    lives = 5; 
+    score = 0; 
     $(".lives").html(lives);
     $(".score").html(score);
     player.reset(); // Reset the player's position
     collectible.setPlacement(); // Reset the collectible's position
     $("#hidden").css("display", "none"); // Hides the game over box
-    $("#hidden").parent().css('z-index', 3000);
+    $("#hidden").parent().css('z-index', 3000); // Brings th game over box back
 })
-
-
-/*
--------- Questions:
-- When the player loses and want to fall off the grid, how to disable setBouondaries?
-- How to center the game over box?
-- How to randomly choose beteen heart and gem to appear?
-- How to stop the none stop log message when the player wins or loses?
-- Is there a better way to randomly select the xy coordinates for the collectible?
-
-*/
